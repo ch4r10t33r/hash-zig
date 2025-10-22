b65ea67a7c89e48beda89833dff9fc1da0a8122c
ch4r10t33r <psramanuj@gmail.com>
2025-10-22 10:57:43 +0100

    fix: Removed cache files

--- benchmark/zig_benchmark/.zig-global-cache/p/zig_poseidon-0.2.0-CHeW2IA_AQCpTqQYrzpREdQX-Yvu4UkX7RuxmMml3Auf/src/fields/babybear/montgomery.zig
+++
@@ -1,1 +0,0 @@
-pub const MontgomeryField = @import("../generic_montgomery.zig").MontgomeryField31(15 * (1 << 27) + 1);
--- benchmark/zig_benchmark/.zig-global-cache/p/zig_poseidon-0.2.0-CHeW2IA_AQCpTqQYrzpREdQX-Yvu4UkX7RuxmMml3Auf/src/fields/koalabear/montgomery.zig
+++
@@ -1,3 +0,0 @@
-// KoalaBear field: p = 2^31 - 2^24 + 1 = 127 * 2^24 + 1 = 2130706433 = 0x7f000001
-// This field is used in plonky3 and Rust hash-sig implementations
-pub const MontgomeryField = @import("../generic_montgomery.zig").MontgomeryField31(127 * (1 << 24) + 1);
